3d11a042126fe5da624746ae17da1339cd07072b
Linfuzi <122346139+Linfuzi@users.noreply.github.com>
2023-05-07 16:33:42 +0800

    0507完成

---
+++ Assets/Scripts/EnemySystem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+/// <summary>
+/// 敵人系統
+/// 1.追蹤玩家
+/// 2.面向玩家
+/// </summary>
+public class EnemySystem : MonoBehaviour
+{
+	[Header("追蹤速度"), Range(0, 100)]
+	public float movespeed;
+	public Transform player;
+	public Animator ani;
+	private void Awake()
+	{
+		player = GameObject.Find("Player").transform;
+	}
+	private void Update()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, player.position, movespeed);
+		ani.SetBool("run", true);
+	}
+}
--- Assets/Scripts/SpawnSysytem.cs
+++ Assets/Scripts/SpawnSysytem.cs
@@ -3,15 +3,16 @@
 public class SpawnSysytem : MonoBehaviour
 {
     [Header("怪物生成間隔時間"), Range(0, 10)]
-    public float interval = 3.5f;
+    public float interval;
     [Header("生成的怪物")]
-    public GameObject prefabsEnemy;
-    void Start()
-    {
-        
-    }
-    void Update()
-    {
-        
-    }
+    public GameObject prefabsenemy;
+
+    private void Monster()
+	{
+        Instantiate(prefabsenemy, transform.position, transform.rotation);
+	}
+	private void Awake()
+	{
+        InvokeRepeating("Monster", 0, interval);
+	}
 }
